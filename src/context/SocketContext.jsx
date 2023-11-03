8442f4e423b793b31423cfced469c2e410423f0f
mrsanchez02 <mrsanchez.dev@gmail.com>
2023-11-03 16:36:29 -0400

    added env dependencies

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -6,7 +6,7 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
 
-  const { socket, online } = useSocket('http://localhost:8080');
+  const { socket, online } = useSocket(import.meta.env.VITE_SOCKET_SERVER);
   
   return (
     <SocketContext.Provider value={{ socket, online }}>
